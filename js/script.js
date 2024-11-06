acdb26ed5486c5d157611341a46e72d24b935d25
unknown <agarridoluengo@gmail.com>
2024-11-06 09:27:53 +0100

    script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
  // Aquí tu código
+let lista = promt('agrega un elemento a la lista');
+const agregar = document.getElementById('agregar');
+let agregarElemento = document.createElement('li')
+
+
+agregarElemento.appendChild(agregarElemento);
+
+agregar.addEventListener('click', function(){
+    agregarElemento.innerHTML = agregarElemento + lista;
+    
+})
