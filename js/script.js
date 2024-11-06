d8e6f092a7ae29f11a4d76a8df4ffa60ee8d7e9a
unknown <agarridoluengo@gmail.com>
2024-11-06 11:33:00 +0100

    index.html

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
  // Aquí tu código
-let lista = promt('agrega un elemento a la lista');
+let lista = document.getElementById('lista');
 const agregar = document.getElementById('agregar');
-let agregarElemento = document.createElement('li')
 
 
-agregarElemento.appendChild(agregarElemento);
+
 
 agregar.addEventListener('click', function(){
-    agregarElemento.innerHTML = agregarElemento + lista;
-    
+    let agregarElemento = prompt('agrega un elemento a la lista');
+    const agregarLista = document.createElement('li');
+    agregarLista.innerText = agregarElemento;
+    lista.appendChild(agregarLista);
 })
